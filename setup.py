5afc4ff28ff383d49a5f178be90ac06e117d9f5b
Rabbi50 <jasrabbi50@gmail.com>
2019-07-21 15:04:54 +0600

    update name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
+    name="sample-pkg-rabbi50",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Rabbi50/sampleProject",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
