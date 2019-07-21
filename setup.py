b12f1ccc460854d5210ed2619d59e48d56d7e234
Rabbi50 <jasrabbi50@gmail.com>
2019-07-21 15:32:38 +0600

    update name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="sample-pkg-rabbi50",
     version="0.0.1",
     author="Example Author",
-    author_email="author@example.com",
+    author_email="jasrabbi50@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
