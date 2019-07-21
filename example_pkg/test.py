360f44920293079ed68210a1dab51a28eb22c08a
Rabbi50 <jasrabbi50@gmail.com>
2019-07-21 16:12:43 +0600

    bal

--- example_pkg/test.py
+++ example_pkg/test.py
@@ -1,1 +1,33 @@
-import cv2
+import nmap
+
+def port_scan(ip_address):
+    # initialize the port scanner
+    nmScan = nmap.PortScanner()
+    # scan localhost for ports in range 21-443
+    nmScan.scan(ip_address)
+    # run a loop to print all the found result about the ports
+    for host in nmScan.all_hosts():
+        print('Host : %s (%s)' % (host, nmScan[host].hostname()))
+        print('State : %s' % nmScan[host].state())
+        for protocol in nmScan[host].all_protocols():
+            print('----------')
+            print('Protocol : %s' % protocol)
+            lport = nmScan[host][protocol].keys()
+            print(lport)
+            # lport.sort()
+            for port in lport:
+                print('port : %s\tstate : %s\treason : %s\tname : %s' % (
+                port, nmScan[host][protocol][port]['state'], nmScan[host][protocol][port]['reason'],
+                nmScan[host][protocol][port]['name']))
+
+
+
+'''
+this code execute only when all_utils.py execute
+'''
+
+if __name__ == "__main__":
+    try:
+        port_scan('167.86.94.236')
+    except:
+        print('Not found any port.')
--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setuptools.setup(
    author='Rabbi',
    author_email='jasrabbi50@gmail.com',
    packages=['example_pkg'],  #same as name
-   install_requires=['opencv-python'], #external packages as dependencies
+   install_requires=['namp'], #external packages as dependencies
 )
